e4a291c2bdcc4132d320e1d34ad13fcfa5dc79fd
Trevor Stephens <treversion@yahoo.com>
2022-06-08 18:59:37 -0700

    adding needed updates to about me page

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # lab-02
 repository for lab2
+
+### Update
+
+Jun 8: extending my about me section
--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 'use strict'
-
+let rightAnswers = 0
 let name = prompt("What is your name?");
 console.log("Users name is " + name);
 alert("Oh Hello " + name);
 
 let myAge = prompt("Do you think I am 33?");
 if (myAge === "yes" || myAge === "Yes" || myAge === "Y" || myAge === "y") {
-    console.log("Wow, " + name + "you are correct!")
+    console.log("Wow, " + name + " you are correct!")
     alert("Wow, " + name + " you are correct!")
+    if (myAge === "yes" || myAge === "Yes" || myAge === "Y" || myAge === "y") {
+        rightAnswers++
+    }
 }
 else {
     myAge === "no" || myAge === "No" || myAge === "N" || myAge === "n"
@@ -15,21 +18,28 @@ else {
     alert("Sorry " + name + " I actually am 33")
 };
 
-let myLocation = prompt("Do you think I am in Seattle?");
+let myLocation = prompt("Do you think I am in San Diego?");
 if (myLocation === "yes" || myLocation === "Yes" || myLocation === "Y" || myLocation === "y") {
-    console.log("Wow, " + name + "you are incorrect!")
-    alert("Wow, " + name + " you are incorrect!")
+    console.log("Wow, " + name + "you are correct!")
+    alert("Wow, " + name + " you are correct!")
+    if (myLocation === "yes" || myLocation === "Yes" || myLocation === "Y" || myLocation === "y") {
+        rightAnswers++
+    }
+
 }
 else {
     myLocation === "no" || myLocation === "No" || myLocation === "N" || myLocation === "n"
-    console.log(name + " How did you know where I am?")
-    alert(name + " How did you know where I am?")
+    console.log(name + " You are incorrect")
+    alert(name + " You are incorrect")
 };
 
-let myLanguage = prompt("Can you use JavaScript");
+let myLanguage = prompt("Do you think I can use JavaScript");
 if (myLanguage === "yes" || myLanguage === "Yes" || myLanguage === "Y" || myLanguage === "y") {
     console.log("Wow, " + name + " I am just learning")
     alert("Wow, " + name + " I am just learning")
+    if (myLanguage === "yes" || myLanguage === "Yes" || myLanguage === "Y" || myLanguage === "y") {
+        rightAnswers++
+    }
 }
 else {
     myLanguage === "no" || myLanguage === "No" || myLanguage === "N" || myLanguage === "n"
@@ -37,25 +47,68 @@ else {
     alert("Sorry " + name + " Well you could start today")
 };
 
-let myGame = prompt("Do you play Xbox?");
+let myGame = prompt("Do I play Xbox?");
 if (myGame === "yes" || myGame === "Yes" || myGame === "Y" || myGame === "y") {
-    console.log("Wow, " + name + "I knew you were cool!")
-    alert("Wow, " + name + " I knew you were cool!")
+    console.log("Wow, " + name + "I do play Xbox")
+    alert("Wow, " + name + " I do play Xbox!")
+    if (myGame === "yes" || myGame === "Yes" || myGame === "Y" || myGame === "y") {
+        rightAnswers++
+    }
 }
 else {
     myGame === "no" || myGame === "No" || myGame === "N" || myGame === "n"
-    console.log("Wow " + name + " that is kind of lame")
-    alert("Wow  " + name + " that is kind of lame")
+    console.log("Wow " + name + " Of course I do!!")
+    alert("Wow  " + name + " Of course I do")
 };
 
-let myWork = prompt(name + " Did you finish your lab today?");
+let myWork = prompt(name + " Did I finish my lab today?");
 if (myWork === "yes" || myWork === "Yes" || myWork === "Y" || myWork === "y") {
     console.log("Wow, " + name + " I barely finished before 11:59")
     alert("Wow, " + name + " I barely finished before 11:59")
+    if (myWork === "yes" || myWork === "Yes" || myWork === "Y" || myWork === "y") {
+        rightAnswers++
+    }
 }
 else {
     myWork === "no" || myWork === "No" || myWork === "N" || myWork === "n"
     console.log("Sorry " + name + " I guess that is 20 pts off")
     alert("Sorry " + name + " I guess that is 20 pts off")
 };
+let correctAnswer = 3;
+for (let i = 0; i <= 3; i++) {
+    console.log(i)
+    let guessedNumber = prompt("I am thinking of a number 1-10")
+    if (guessedNumber == correctAnswer) {
+        alert("You are correct!");
+        if (guessedNumber == correctAnswer) { rightAnswers++ };
+        break;
+    }
+    if (guessedNumber < correctAnswer) { alert("Your answer is too low") }
+    if (guessedNumber > correctAnswer) { alert("Your number is too high!") }
+    if (i == 3) { alert("done") }
+}
+let games = ["Halo", "Apex", "Pokemon", "Destiny"];
+let attempts = 6;
+console.log(games)
+while (attempts > 0) {
+    attempts--;
+    console.log("attempts left ", attempts);
+    let correctAnswer = false
 
+    let guessedGame = prompt("what is one of my favorite games?")
+    for (let i = 0; i <= games.length; i++) {
+        console.log(i);
+        if (guessedGame == games[i]) {
+            alert("You are correct!");
+            correctAnswer = true
+            attempts = 0
+            if (guessedGame == games[i]) {
+                rightAnswers++
+            }
+            break;
+        }
+    }
+    if (!correctAnswer) { alert("please try again") }
+
+}
+alert("you got " + rightAnswers + " right answers")
